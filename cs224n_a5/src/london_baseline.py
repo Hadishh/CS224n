bf0ae4956b434503b385dfd7d76811457f3748ff
Hadi Sheikhi <hadsheikhi@gmail.com>
2021-06-06 20:41:35 +0430

    implement london predictions

--- cs224n_a5/src/london_baseline.py
+++ cs224n_a5/src/london_baseline.py
@@ -2,3 +2,11 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+import utils
+total_length = 0
+with open("birth_dev.tsv") as tsv:
+    lines = tsv.readlines()
+    total_length = len(lines)
+
+accuracy = utils.evaluate_places("birth_dev.tsv", ["London"] * total_length)
+print(f"Accuray is : {accuracy}")
